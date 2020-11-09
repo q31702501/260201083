a45d806ea228ad89e80873bced3e09295e709c8c
q31702501 <mehmetokay@std.iyte.edu.tr>
2020-11-09 09:36:13 +0000

    lab4 factorial

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -5,6 +5,7 @@ num=str(num)
 two_digit=[]
 for i in range(len(num),len(num)-2,-1):
 """
+""""
 a=int(input("a:"))
 b=int(input("b:"))
 new_a=a
@@ -12,3 +13,11 @@ for i in range(b-1):
   new_a=new_a*a
 
 print(new_a)
+"""
+factorial=1
+num=int(input("number:"))
+
+for i in range(1,num+1):
+  factorial=factorial*i
+
+print(factorial)
